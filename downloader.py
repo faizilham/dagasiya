d96e6dac413935f07d191ee0c756d0f3098b47a0
Faiz Ilham <indigo.republica@gmail.com>
2015-09-13 08:16:46 +0700

    add database updates to download

--- downloader.py
+++ downloader.py
@@ -3,16 +3,18 @@ import requests
 import os
 
 CHUNKSIZE = 1024 * 1024
+WORKER_NUMBER = 4
 
 class Downloader:
-	def __init__(self, config, servers, users, downloads):
+	def __init__(self, config, servers, users, downloads, dblock):
 		self.servers = servers
 		self.users = users
 		self.downloads = downloads
 		self.download_folder = config["download_folder"]
 		self.finished_folder = config["finished_folder"]
+		self.dblock = dblock
 
-	def start(self, servername, filename, user):
+	def start(self, filename, servername, user):
 		
 		if self.servers.protocol(servername) == "http":
 
@@ -25,7 +27,8 @@ class Downloader:
 			### if not exist, mark error
 			if res.status_code != 200: 
 				print "error: ", res.status_code, filename
-				self.downloads.update_error(filename, servername)
+				with self.dblock:
+					self.downloads.update_error(filename, servername)
 				return
 
 			### create before multiple writes
@@ -37,8 +40,13 @@ class Downloader:
 
 			print "Downloading", filename, "from server", servername, "to", filepath
 
-			### count and distribute chunk tasks
+			### count bytes and mark download
 			nbytes = int(res.headers["content-length"])
+			with self.dblock:
+				self.downloads.update_download(filename, servername, user)
+				self.users.add_usage(user, nbytes)
+
+			### distribute chunk tasks
 			chunks = [{"pos": k, "start": i, "end": min(i+CHUNKSIZE, nbytes-1), "url":url, "filepath": filepath, "headers": headers}  for k, i in enumerate(range(0, nbytes, CHUNKSIZE))]
 
 			### create status file if not exist
@@ -48,10 +56,18 @@ class Downloader:
 				with open(filepath + ".status", "wb") as stat:
 					stat.write('\0' * len(chunks))
 
+			# start task to 
 			workers = Pool(4)
-			workers.map(download_chunk, chunks)
+			success = all(workers.map(download_chunk, chunks))
 
-			os.rename(filepath, self.finished_folder + filename)
+			
+			if success:
+				os.rename(filepath, self.finished_folder + filename)
+				with self.dblock: self.downloads.update_finish(filename, servername)
+			else:
+				os.remove(filepath)
+				with self.dblock: self.downloads.update_error(filename, servername)
+
 			os.remove(filepath + ".status")
 
 def download_chunk(data):
